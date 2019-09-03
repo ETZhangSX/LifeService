55c9de31fc8d89194ed63c0d77bec647feec90fa
ETZhangSX <etzhangsx@qq.com>
2019-09-03 21:50:54 +0800

    移除配置拉取代码, 添加配置模块引入

--- InterfaceServer/app.js
+++ InterfaceServer/app.js
@@ -1,6 +1,17 @@
-// const https = require('https');
 const Koa     = require('koa');
 const koaBody = require('koa-bodyparser');
+const https   = require('https');
+const fs      = require('fs');
+const appConf = require('./config/appConf');
+
+// 证书密钥路径
+var crt_path = appConf.crt_path;
+var key_path = appConf.key_path;
+
+// 读取证书密钥
+var crt = fs.readFileSync(crt_path, 'utf-8');
+var key = fs.readFileSync(key_path, 'utf-8');
+const options = {key: key, cert: crt};
 
 // 创建KOA实例
 const app = new Koa();
@@ -23,6 +34,10 @@ app.use(apiRouter.routes());
 app.host = hostname;
 app.port = port;
 
-const server = app.listen(app.port, app.host, () => {
-    console.log('Koa server listening on %s %d', server.address().address, server.address().port);
+// const server = app.listen(app.port, app.host, () => {
+//     console.log('Koa server listening on %s %d', server.address().address, server.address().port);
+// });
+
+const https_server = https.createServer(options, app.callback()).listen(app.port, app.host, () => {
+    console.log('Koa server listening on %s %d', https_server.address().address, https_server.address().port);
 });
