bc9f3da5dfbc8d6ba2d0a25026ca03c92d313add
ETZhangSX <etzhangsx@qq.com>
2019-09-06 14:56:13 +0800

    添加配置信息，修复拉取配置信息失败导致的服务无法启动

--- InterfaceServer/config/appConf.js
+++ InterfaceServer/config/appConf.js
@@ -1,22 +1,27 @@
 const TarsConfig = require('@tars/config');
-// 实例化
-var config = new TarsConfig();
 
 const appConf = {
-    crt_path: '',
-    key_path: '',
-    app_id: '',
-    app_secret: '',
+    crt_path: '/home/1_www.etzhang.xyz_bundle.crt',
+    key_path: '/home/2_www.etzhang.xyz.key',
+    app_id: 'wx2f3efb98397488ae',
+    app_secret: '386f75a1271dfdbddd18d41d2336f007',
+    // 拉取配置
+    loadConfig: function() {
+        var that = this;
+        // 实例化
+        var config = new TarsConfig();
+        
+        config.loadConfig("LifeService.MessageWallServer.conf", {format: config.FORMAT.JSON})
+        .then(function(data) {
+            that.crt_path   = data.crt_path;
+            that.key_path   = data.key_path;
+            that.app_id     = data.app_id;
+            that.app_secret = data.app_secret;
+            console.log(data);
+        }, function (err) {
+            console.error("loadConfig err", err);
+        });
+    }
 };
-
-// 拉取配置
-config.loadConfig("LifeService.MessageWallServer.conf", {format: config.FORMAT.JSON}).then(function(data) {
-    appConf.crt_path   = data.crt_path;
-    appConf.key_path   = data.key_path;
-    appConf.app_id     = data.app_id;
-    appConf.app_secret = data.app_secret;
-}, function (err) {
-    console.error("loadConfig err", err);
-});
 
 module.exports = appConf;
