9551f8a4f399d830da62b48243fff7664a344608
ETZhangSX <etzhangsx@qq.com>
2019-09-09 10:46:35 +0800

    添加join语句构建函数

--- DataServer/util.cpp
+++ DataServer/util.cpp
@@ -80,3 +80,54 @@ string buildSelectSQL(const string &sTableName,const string &sColumn, const stri
     }
     return os.str();
 }
+
+string buildJoinSQL(const string &sTableLeft, const string &sTableRight, const MYSQL_JoinType &joinType, const vector<string> &sColumns, const string sOnFilter, const string &orderBy, const MYSQL_Order &mysqlOrder, const int &limitNum)
+{
+    ostringstream sColumnNameSet;
+
+    vector<string>::const_iterator itEnd = sColumns.end();
+
+    for(vector<string>::const_iterator it = sColumns.begin(); it != itEnd; ++it)
+    {
+        if (it == sColumns.begin())
+        {
+            sColumnNameSet << *it;
+        }
+        else
+        {
+            sColumnNameSet << ", " << *it;
+        }
+    }
+
+    ostringstream os;
+    os << "select " << sColumnNameSet.str()
+       << " from " << sTableLeft;
+    
+    if (joinType == INNERJOIN)
+        os << " inner join ";
+    else if (joinType == LEFTJOIN)
+        os << " left join ";
+    else if (joinType == RIGHTJOIN)
+        os << " right join ";
+
+    os << sTableRight;
+    
+    if (sOnFilter != "")
+       os << " on " << sOnFilter;
+
+    if (orderBy != "")
+    {
+        os << " order by `" << orderBy << "`";
+        if (mysqlOrder == ASC)
+            os << " ASC";
+        else if (mysqlOrder == DESC)
+            os << " DESC";
+    }
+
+    if (limitNum != 0)
+    {
+        os << " limit " << tars::TC_Common::tostr<int>(limitNum);
+    }
+
+    return os.str();
+}
--- DataServer/util.h
+++ DataServer/util.h
@@ -9,6 +9,13 @@ enum MYSQL_Order
     ASC,
     DESC
 };
+
+enum MYSQL_JoinType
+{
+    INNERJOIN,
+    LEFTJOIN,
+    RIGHTJOIN
+};
 /**
  * @brief 构建查询语句
  * 
@@ -58,3 +65,19 @@ std::string buildSelectSQL(const std::string &sTableName, const std::string &sCo
  * @return string   SQL语句
  */
 std::string buildSelectSQL(const std::string &sTableName, const std::string &sColumn, const std::string &sWhereFilter, const std::string &orderBy, const MYSQL_Order &mysqlOrder, const int &limitNum);
+
+/**
+ * @brief 构建Join查询语句
+ * 
+ * @param sTableLeft    左表名
+ * @param sTableRight   右表名
+ * @param joinType      join类型
+ * @param sColumn       列名(单个)
+ * @param sOnFilter     on范围过滤
+ * @param orderBy       排序字段
+ * @param mysqlOrder    排序方式 ASC, DESC
+ * @param limitNum      限制查询条数
+ * 
+ * @return string   SQL语句
+ */
+std::string buildJoinSQL(const std::string &sTableLeft, const std::string &sTableRight, const MYSQL_JoinType &joinType, const vector<std::string> &sColumns, const std::string sOnFilter, const std::string &orderBy, const MYSQL_Order &mysqlOrder, const int &limitNum);
