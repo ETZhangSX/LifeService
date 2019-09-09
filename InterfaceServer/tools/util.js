c8d8f874cef6e8b6d82cc2d993e92c7d7abf97c2
ETZhangSX <etzhangsx@qq.com>
2019-09-09 22:19:40 +0800

    添加常量

--- InterfaceServer/tools/util.js
+++ InterfaceServer/tools/util.js
@@ -3,7 +3,9 @@
 var DataHandle = DataHandle || {};
 module.exports.DataHandle = DataHandle;
 
-DataHandle.returnData = function(status, errmsg, data = "") {
+DataHandle.Success = 'success';
+
+DataHandle.returnData = function(status, errmsg, data = {}) {
     return {
         'status': status,
         'errmsg': errmsg,
