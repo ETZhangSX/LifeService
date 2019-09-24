aca60af3eafb30f0d5282be7ace4b4a9dcd92da9
ETZhangSX <etzhangsx@qq.com>
2019-09-24 15:42:38 +0800

    修改返回数据构建模块, 添加对服务状态码的处理逻辑

--- InterfaceServer/tools/util.js
+++ InterfaceServer/tools/util.js
@@ -1,17 +1,23 @@
 /* eslint-disable */
+const errorCode = require('../proxy/ServerStatusTars').LifeService.ErrorCode;
 
 var DataHandle = DataHandle || {};
 module.exports.DataHandle = DataHandle;
 
-DataHandle.Success = 'success';
+DataHandle.returnData = function(status, data = {}) {
+    if (status == errorCode.SUCCESS)
+        return this.returnSuccess(status, 'success', data);
+    let errmsg = this._getErrMsg(status);
+    return this.returnError(status, errmsg);
+};
 
-DataHandle.returnData = function(status, errmsg, data = {}) {
+DataHandle.returnSuccess = function(status, errmsg, data = {}) {
     return {
         'status': status,
         'errmsg': errmsg,
           'data':   data,
     }
-};
+}
 
 DataHandle.returnError = function(status, errmsg) {
     return {
@@ -19,3 +25,38 @@ DataHandle.returnError = function(status, errmsg) {
         'errmsg': errmsg,
     }
 };
+
+DataHandle._getErrMsg = function(errCode) {
+    switch(errCode) {
+        case errorCode.USEREXIST:
+            return "User exist";
+        case errorCode.PHONEEXIST:
+            return "Phone number exist";
+        case errorCode.CLUBEXIST:
+            return "Club exist";
+        case errorCode.MANAGEREXIST:
+            return "Manager exist";
+        case errorCode.USERAPPLIED:
+            return "User has applied";
+        case errorCode.ACTIVITYEXIST:
+            return "Activity exist";
+        case errorCode.RECORDEXIST:
+            return "User has already applied activity";
+        case errorCode.USERNOTEXIST:
+            return "User does not exist";
+        case errorCode.CLUBNOTEXIST:
+            return "Club does not exist";
+        case errorCode.MANAGERNOTEXIST:
+            return "Manager does not exist";
+        case errorCode.ACTIVITYNOTEXIST:
+            return "Activity does not exist";
+        case errorCode.APPLYNOTEXIST:
+            return "Application for club does not exist";
+        case errorCode.RECORDNOTEXIST:
+            return "Activity record does not exist";
+        case errorCode.SERVERERROR:
+            return "Server ERROR";
+        default:
+            return "Unknow ERROR"
+    }
+}
